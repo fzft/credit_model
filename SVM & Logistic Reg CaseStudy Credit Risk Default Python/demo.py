d512d4e7785b5b31781ab4934507089e76dce1b1
fangzhenfutao <fangzhenfutao@cardvalue.cn>
2020-07-19 00:09:36 +0800

    model preprocessing and feature engineering

--- SVM & Logistic Reg CaseStudy Credit Risk Default Python/demo.py
+++ SVM & Logistic Reg CaseStudy Credit Risk Default Python/demo.py
@@ -1,11 +1,7 @@
+import matplotlib.pyplot as plt
+from sklearn.preprocessing import PolynomialFeatures
 import numpy as np
 
-a = np.arange(36).reshape(4, 9)
 
-n_samples, n_features = a.shape
-K = np.zeros((n_samples, n_samples))
-for i, x_i in enumerate(a):
-    for j, x_j in enumerate(a):
-        K[i, j] = np.inner(x_i, x_j)
 
-print(K)
+
