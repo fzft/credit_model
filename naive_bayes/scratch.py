95bbf1ac5d5838afe8c4698408cb170e41d59b54
fangzhenfutao <fangzhenfutao@cardvalue.cn>
2020-07-18 15:39:27 +0800

    naive bayes

--- naive_bayes/scratch.py
+++ naive_bayes/scratch.py
@@ -1,53 +1,61 @@
 import numpy as np
-import pandas as pd
-from typing import Callable
-
-from sklearn.feature_extraction.text import CountVectorizer
+from scipy.stats import norm
 
 
-def make_spam_dataset(show_X=True) -> (pd.DataFrame, np.ndarray, Callable):
-    vocab = [
-        'secret', 'offer', 'low', 'price', 'valued', 'customer', 'today',
-        'dollar', 'million', 'sports', 'is', 'for', 'play', 'healthy', 'pizza'
-    ]
-
-    spam = [
-        'million dollar offer',
-        'secret offer today',
-        'secret is secret'
-    ]
-
-    not_spam = [
-        'low price for valued customer',
-        'play secret sports today',
-        'sports is healthy',
-        'low price pizza'
-    ]
-
-    all_messages = spam + not_spam
-
-    vectorizer = CountVectorizer(vocabulary=vocab)
-    word_counts = vectorizer.fit_transform(all_messages).toarray()
-    df = pd.DataFrame(word_counts, columns=vocab)
-    is_spam = [1] * len(spam) + [0] * len(not_spam)
-    msg_tx_func = lambda x: vectorizer.transform(x).toarray()
-
-    if show_X:
-        print(df)
-
-    return df.to_numpy(), np.array(is_spam), msg_tx_func
+def separate_by_class(dataset):
+    separated = dict()
+    for i in range(len(dataset)):
+        vector = dataset[i]
+        class_value = vector[-1]
+        if (class_value not in separated):
+            separated[class_value] = list()
+        separated[class_value].append(vector)
+    return separated
 
 
-# X, y, tx_func = make_spam_dataset()
+# Test separating data by class
+dataset = [[3.393533211, 2.331273381, 0],
+           [3.110073483, 1.781539638, 0],
+           [1.343808831, 3.368360954, 0],
+           [3.582294042, 4.67917911, 0],
+           [2.280362439, 2.866990263, 0],
+           [7.423436942, 4.696522875, 1],
+           [5.745051997, 3.533989803, 1],
+           [9.172168622, 2.511101045, 1],
+           [7.792783481, 3.424088941, 1],
+           [7.939820817, 0.791637231, 1]]
+separated = separate_by_class(dataset)
 
 
-class NaiveBayes(object):
-    """ DIY binary Naive Bayes classifier based on categorical data """
+# Calculate the mean, stdev and count for each column in a dataset
+def summarize_dataset(dataset):
+    summaries = [(np.mean(column), np.std(column), len(column)) for column in zip(*dataset)]
+    del (summaries[-1])
+    return summaries
 
-    def __init__(self, alpha=1.0):
-        """ """
-        self.prior = None
-        self.word_counts = None
-        self.lk_word = None
-        self.alpha = alpha
-        self.is_fitted_ = False
+
+def calculate_probability(x, mean, stdev):
+    return norm.pdf(x, mean, stdev)
+
+
+def calculate_class_probabilities(summaries, row):
+    total_rows = sum([summaries[label][0][2] for label in summaries])
+    probabilities = dict()
+    for class_value, class_summaries in summaries.items():
+        probabilities[class_value] = summaries[class_value][0][2] / float(total_rows)
+        for i in range(len(class_summaries)):
+            mean, stdev, _ = class_summaries[i]
+            probabilities[class_value] *= calculate_probability(row[i], mean, stdev)
+    return probabilities
+
+
+def summarize_by_class(dataset):
+    separated = separate_by_class(dataset)
+    summaries = dict()
+    for class_value, rows in separated.items():
+        summaries[class_value] = summarize_dataset(rows)
+    return summaries
+
+
+summaries = summarize_by_class(dataset)
+print(summaries)
